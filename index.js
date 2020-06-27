428313e269188af444ff32d94b21f7619eadd169
Soumen Khara <Soumen2015.s.k@gmail.com>
2020-06-27 16:40:47 +0530

    :heart: Setup the server and routes using Express

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const routes = require('./routes')
+const PORT = process.env.PORT || 8080
+
+app.use(express.static('static'))
+app.use(express.json())
+
+app.get('/emojis', routes)
+
+app.listen(PORT, e => { console.log('Server started on port ' + PORT) })
